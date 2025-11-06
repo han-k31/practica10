f83709c16494d912ddb746b2aa523c1d8fa14415
han-k31 <hannasgiron@gmail.com>
2025-11-06 13:41:46 -0600

    Create ejercicio6.c

---
+++ ejercicio6.c
@@ -0,0 +1,24 @@
+/* Girón Knepp Hanna Sofía
+laboratorio 10 ejercicio 2.c*/
+
+#include<stdio.h> 
+int main() 
+{ 
+    int i,j,a[5][5]; 
+    i=0;   
+    do  //Representa al renglón del arreglo 
+    { 
+        j=0; 
+        do //Representa a la columna del arreglo 
+        { 
+           a[i][j]=i+j; 
+           printf("\t%d, ",a[i][j]); 
+           j++; 
+        } 
+        while (j<5); 
+        printf("\n"); 
+        i++; 
+    } 
+    while (i<5); 
+    return 0; 
+}
