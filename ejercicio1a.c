c705bc803148acb176b9c64bbc0e2521293d22ef
han-k31 <hannasgiron@gmail.com>
2025-11-06 13:20:25 -0600

    Create ejercicio1a.c

---
+++ ejercicio1a.c
@@ -0,0 +1,20 @@
+/* Girón Knepp Hanna Sofía
+laboratorio 10 ejercicio 1*/
+
+#include<stdio.h> 
+int main() 
+{ 
+    int matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}}; 
+    int i, j; 
+    printf("Imprimir Matriz\n"); 
+    for (i=0 ; i<3 ; i++) //Representa al renglón del arreglo 
+    { 
+        for (j=0 ; j<3 ; j++)//Representa a la columna del arreglo 
+        { 
+            printf("%d, ",matriz[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+ 
+    return 0; 
+}
